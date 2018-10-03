d96a8fc57302443f76c9377a3708e8322243b1e5
egonm12 <egonmeijers12@gmail.com>
2018-10-03 13:44:02 +0200

    Changed class name in devise config for mail

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # with default "from" parameter.
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'CustomDeviseMailer'
+  config.mailer = 'KudosDeviseMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
